5c90b03654f6953e8d213db01a49e34ce219bbd7
西川雅人 <masato.nishikawa@arteryex.biz>
2025-04-14 17:36:36 +0900

    CORS（クロスオリジンリソース共有）を有効化

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // これがFlutterとの通信に必要！
+  app.enableCors(); 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
